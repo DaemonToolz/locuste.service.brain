a526d91ee18c661b3ade72e72b18de8720b570b3
Axel MACIEJEWSKI <axel.maciejewski@viacesi.fr>
2020-05-27 16:10:04 +0200

    feat(ipc): added first round of ipc with diagnostic tool

--- pipe_main.go
+++ pipe_main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"io"
 	"log"
 	"os"
+	"syscall"
 	"time"
 )
 
@@ -12,29 +14,37 @@ var ongoingDiagProcess bool
 
 // Diagnostic code
 func pipeMain() {
-	log.Println("Ouverture de la pipe nommée: locuste.brain.diagnostic")
-	readPipe, _ := os.OpenFile("locuste.brain.diagnostic", os.O_RDONLY, 0600)
-	defer readPipe.Close()
-	writePipe, _ := os.OpenFile("locuste.diagnostic.brain", os.O_WRONLY, 0600)
-	defer writePipe.Close()
+	log.Println("Ouverture de la pipe nommée pour locuste.service.brain")
+	syscall.Mkfifo("/tmp/locuste.brain.diagnostic", 0666)
+	syscall.Mkfifo("/tmp/locuste.diagnostic.brain", 0666)
 
-	var buffer bytes.Buffer
+	//defer readPipe.Close()
+	//writePipe, _ := os.OpenFile("/tmp/locuste.diagnostic.brain", os.O_WRONLY, os.ModeNamedPipe)
+	//defer writePipe.Close()
+
 	log.Println("En attente d'instruction de diagnostiques")
 	ongoingDiagProcess = true
 
 	for ongoingDiagProcess == true {
+		var buffer bytes.Buffer
+		readPipe, _ := os.OpenFile("/tmp/locuste.brain.diagnostic", os.O_RDONLY, os.ModeNamedPipe)
+		writePipe, _ := os.OpenFile("/tmp/locuste.diagnostic.brain", os.O_WRONLY, os.ModeNamedPipe)
 		_, err := io.Copy(&buffer, readPipe)
-
 		if buffer.Len() > 0 {
 			log.Println("Instruction de diagnostique ", buffer.String())
+			writePipe.WriteString(fmt.Sprintf("Instruction %s recue", buffer.String()))
+			log.Println("Réponse envoyée")
 		}
 
 		if err != nil {
+			failOnError(err, "Une erreur est survenue")
 			if err.Error() != "EOF" {
-				return
+				readPipe.Close()
+				break
 			}
 		}
-
+		readPipe.Close()
+		writePipe.Close()
 		time.Sleep(250 * time.Millisecond)
 	}
 
