f5fa569fd19b47207173a3daa7328aee8f09ac4a
Axel MACIEJEWSKI <axel.maciejewski@viacesi.fr>
2020-06-01 11:49:48 +0200

    feat(pipes): added func mapping

--- pipe_main.go
+++ pipe_main.go
@@ -6,48 +6,137 @@ import (
 	"io"
 	"log"
 	"os"
+	"strconv"
+	"strings"
+	"sync"
 	"syscall"
 	"time"
 )
 
 var ongoingDiagProcess bool
 
+// commandAssociation On bind les informations reçues avec
+var commandAssociation map[string](map[string]interface{}) // On ajouter le boolean pour indiquer si on a un string param
+
+func initCommandAssociation() {
+	commandAssociation = make(map[string](map[string]interface{}))
+	commandAssociation["list"] = map[string]interface{}{
+		"func":         listModules,
+		"string_param": false,
+	}
+	commandAssociation["start"] = map[string]interface{}{
+		"func":         moduleStart,
+		"string_param": true,
+	}
+
+	commandAssociation["restart"] = map[string]interface{}{
+		"func":         moduleRestart,
+		"string_param": true,
+	}
+	commandAssociation["stop"] = map[string]interface{}{
+		"func":         moduleStop,
+		"string_param": true,
+	}
+
+}
+
+var readPipe *os.File
+var writePipe *os.File
+var dataMutex sync.Mutex
+var sharedData []string
+
+// AddPipeSharedData Permet d'ajouter une information en provenance de la pipe entrante
+func AddPipeSharedData(input string) {
+	dataMutex.Lock()
+	sharedData = append(sharedData, input)
+	dataMutex.Unlock()
+}
+
+// GetFirstPipeSharedData Récupère la plus vieille instruction disponible
+func GetFirstPipeSharedData() string {
+	var firstInstruction string
+	dataMutex.Lock()
+	firstInstruction, sharedData = sharedData[0], sharedData[1:]
+	dataMutex.Unlock()
+	return firstInstruction
+}
+
 // Diagnostic code
 func pipeMain() {
 	log.Println("Ouverture de la pipe nommée pour locuste.service.brain")
 	syscall.Mkfifo("/tmp/locuste.brain.diagnostic", 0666)
 	syscall.Mkfifo("/tmp/locuste.diagnostic.brain", 0666)
-
-	//defer readPipe.Close()
-	//writePipe, _ := os.OpenFile("/tmp/locuste.diagnostic.brain", os.O_WRONLY, os.ModeNamedPipe)
-	//defer writePipe.Close()
-
-	log.Println("En attente d'instruction de diagnostiques")
+	initCommandAssociation()
 	ongoingDiagProcess = true
-
-	for ongoingDiagProcess == true {
-		var buffer bytes.Buffer
-		readPipe, _ := os.OpenFile("/tmp/locuste.brain.diagnostic", os.O_RDONLY, os.ModeNamedPipe)
-		writePipe, _ := os.OpenFile("/tmp/locuste.diagnostic.brain", os.O_WRONLY, os.ModeNamedPipe)
-		_, err := io.Copy(&buffer, readPipe)
-		if buffer.Len() > 0 {
-			log.Println("Instruction de diagnostique ", buffer.String())
-			writePipe.WriteString(fmt.Sprintf("Instruction %s recue", buffer.String()))
-			log.Println("Réponse envoyée")
-		}
-
-		if err != nil {
-			failOnError(err, "Une erreur est survenue")
-			if err.Error() != "EOF" {
-				readPipe.Close()
-				break
-			}
-		}
-		readPipe.Close()
-		writePipe.Close()
-		time.Sleep(250 * time.Millisecond)
-	}
+	log.Println("En attente d'instruction de diagnostiques")
+	go startReadProcess()
+	go startWriteProcess()
 
 	log.Println("Arrêt du module de diagnostique")
+
+}
+
+func startReadProcess() {
+	for ongoingDiagProcess == true {
+		var buffer bytes.Buffer
+		readPipe, _ = os.OpenFile("/tmp/locuste.brain.diagnostic", os.O_RDONLY, os.ModeNamedPipe)
+		io.Copy(&buffer, readPipe)
+		if buffer.Len() > 0 {
+			AddPipeSharedData(buffer.String())
+		}
+		readPipe.Close()
+		time.Sleep(250 * time.Millisecond)
+	}
+}
+
+func startWriteProcess() {
+	for ongoingDiagProcess == true {
+		writePipe, _ := os.OpenFile("/tmp/locuste.diagnostic.brain", os.O_WRONLY, os.ModeNamedPipe)
+		if len(sharedData) > 0 {
+			executeDiagnosticFunction(writePipe, GetFirstPipeSharedData())
+		}
+		writePipe.Close()
+		time.Sleep(300 * time.Millisecond)
+	}
+}
+
+func executeDiagnosticFunction(output *os.File, input string) {
+	log.Println("Instruction de diagnostique : ", input)
+	if strings.Count(input, " ") >= 1 {
+		dataStr := strings.Split(input, " ")
+
+		if funcMapper, ok := commandAssociation[dataStr[1]]; ok {
+			hasStrParam := funcMapper["string_param"].(bool)
+			if hasStrParam && len(dataStr) > 2 {
+				funcMapper["func"].(func(*os.File, string))(output, dataStr[2])
+			} else if !hasStrParam {
+				funcMapper["func"].(func(*os.File))(output)
+			}
+		}
+
+	}
+	log.Println("Réponse envoyée")
+
+}
+
+func listModules(output *os.File) {
+	outputStr := ""
+	for key, value := range GlobalStatuses {
+		outputStr += fmt.Sprintf("[%s]:%s,", string(key), strconv.FormatBool(value))
+	}
+
+	output.WriteString(outputStr)
+	log.Println("Informations envoyées " + outputStr)
+}
+
+func moduleStart(output *os.File, input string) {
+
+}
+
+func moduleRestart(output *os.File, input string) {
+
+}
+
+func moduleStop(output *os.File, input string) {
 
 }
